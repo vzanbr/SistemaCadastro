e0392d329d8d177af42d6fc49e16f70401ec6341
vzanbr <gabriecvr123@gmail.com>
2024-03-15 18:51:08 -0300

    Feat : Colocando resposta de alguns erros!

--- src/main/java/br/com/sistemacadastro/rest/dto/ServicoPrestadoDTO.java
+++ src/main/java/br/com/sistemacadastro/rest/dto/ServicoPrestadoDTO.java
@@ -3,11 +3,21 @@ package br.com.sistemacadastro.rest.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 }
