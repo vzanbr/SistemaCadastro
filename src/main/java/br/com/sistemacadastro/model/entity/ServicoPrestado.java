7d99f6e2a12181d507c22e0c377d28affd1bd6be
vzanbr <gabriecvr123@gmail.com>
2024-03-15 18:51:22 -0300

    Feat : Colocando anotações para teste

--- src/main/java/br/com/sistemacadastro/model/entity/ServicoPrestado.java
+++ src/main/java/br/com/sistemacadastro/model/entity/ServicoPrestado.java
@@ -2,13 +2,19 @@ package br.com.sistemacadastro.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class ServicoPrestado {
 
     @Id
