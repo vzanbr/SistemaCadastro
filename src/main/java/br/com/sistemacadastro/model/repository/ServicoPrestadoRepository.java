7bf3a180119671b1e7c724af3a33442f5d60c74d
vzanbr <gabriecvr123@gmail.com>
2024-03-15 12:39:45 -0300

    Feat : criado Query

--- src/main/java/br/com/sistemacadastro/model/repository/ServicoPrestadoRepository.java
+++ src/main/java/br/com/sistemacadastro/model/repository/ServicoPrestadoRepository.java
@@ -2,6 +2,15 @@ package br.com.sistemacadastro.model.repository;
 
 import br.com.sistemacadastro.model.entity.ServicoPrestado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer> {
+
+    @Query("select s from ServicoPrestado s join s.cliente c where upper(c.nome) like upper(:nome) and MONTH(s.data) = :mes " )
+    List<ServicoPrestado> findByNomeClienteAndMes
+            (@Param("nome") String nome,
+             @Param("mes") String mes);
 }
